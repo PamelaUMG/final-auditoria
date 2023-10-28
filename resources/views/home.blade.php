f4b0a80af145cff81a51e344ee136c083df24551
PamelaUMG <spamela2001@gmail.com>
2023-10-28 09:04:17 -0600

    segundo comitt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h3 class="page__heading">Diana Seguridad y Auditoria</h3>
+            <h3 class="page__heading">Seguridad y Auditoria</h3>
         </div>
         <div class="section-body">
             <div class="row">
